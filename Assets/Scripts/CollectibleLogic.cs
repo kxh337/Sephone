3aa3065b02789baa92d1eed98e4db7861a5ee259
Jiawei He <jxh602@case.edu>
2014-09-11 16:42:21 -0400

    Basic gameplay scripts
    A few script files that allow a player to pick up an item, and gives the
    player "momentum" to push objects around.

---
+++ Assets/Scripts/CollectibleLogic.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleLogic : MonoBehaviour {
+
+	void itemCollected() 
+	{
+		//Could play sounds of item being picked up here, or trigger some event
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PickUp : MonoBehaviour {
+
+
+	private int[] inventoryArray = {1};
+	public GameObject inventoryText;
+	private float distance = 3.0F;
+
+
+	void pickUpItem() 
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+		{
+
+			if(hit.distance < distance)
+			{
+				inventoryArray[0]++;
+				hit.transform.SendMessage("itemCollected", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+	void Update() 
+	{
+		inventoryText.guiText.text = "Cubes Collected: " + inventoryArray[0];
+		if (Input.GetButtonDown("Fire1")) 
+		{
+
+			pickUpItem();
+		}
+	}
+}
