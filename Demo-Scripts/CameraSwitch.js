5304e791e987d42a76417a35ea1bc1f46e04f8e0
Jiawei He <jxh602@case.edu>
2014-10-11 15:30:38 -0400

    Scripts for the demo.
    Revised scripts and 2 camera scripts to be put into the demo scene.

---
+++ Demo-Scripts/CameraSwitch.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+var camera1 : Camera;
+var camera2 : Camera;
+var animationDuration : float;
+
+//private var boxPhysics : BoxPhysics;
+
+function Start() {
+	GameObject.Find("Player").GetComponent(FPSInputController).enabled = false;
+	GameObject.Find("Graphics").GetComponent(MeshRenderer).enabled = false;
+	camera1.camera.enabled = false;
+	camera2.camera.enabled = true;
+	camera2.animation.Play();
+	
+	yield WaitForSeconds(animationDuration);
+	if(camera2.animation.isPlaying == false)
+	{
+		mainCameraSwitch();
+	}
+	
+	/*boxPhysics = GameObject.Find("ExplosionOrigin").GetComponent(BoxPhysics);
+	
+	yield WaitForSeconds(2);
+	moveBoxes();*/
+}	
+
+
+
+/*function moveBoxes() {
+	boxPhysics.Explode();
+}*/
+
+function mainCameraSwitch() 
+{
+	camera1.camera.enabled = true;
+	camera2.camera.enabled = false;
+	GameObject.Find("Player").GetComponent(FPSInputController).enabled = true;
+	GameObject.Find("Graphics").GetComponent(MeshRenderer).enabled = true;
+}
