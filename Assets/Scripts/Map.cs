d4891e589d062247264875e7ec42f0872d6fe034
Matheus Portillo <mathportillo@gmail.com>
2014-11-09 15:12:08 -0500

    adding new map algorithm

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,69 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Map : MonoBehaviour {
+public class Map2 : MonoBehaviour {
 	public Texture2D mapSource;
-	public Texture2D mapCoverSource;
 	public int width = 256;
 	public int height = 256;
 	public int visionPercentage = 3;
 	
 	Texture2D map;
-	Texture2D mapCover;
+	Color32[] bitmap;
+	int[] alpha;
 	float refWidth;
 	float refHeight;
 	Rect position;
 	int visionPixels;
 
+	Vector3 PlayerPos;
+	Bounds refObjBounds;
+
 	void Start() {
 		if (mapSource == null) {
 			mapSource = new Texture2D (width, height);
 		}
-		if (mapCoverSource == null) {
-			mapCoverSource = new Texture2D (width, height);
-		}
+		bitmap = mapSource.GetPixels32();
+		PlayerPos = GameObject.FindWithTag ("Player").transform.position;
 
 		map = new Texture2D(mapSource.width, mapSource.height);
-		map.SetPixels32(mapSource.GetPixels32());
+		map.SetPixels32(bitmap);
 		map.Apply();
-		
-		mapCover = new Texture2D(mapCoverSource.width, mapCoverSource.height);
-		mapCover.SetPixels32(mapCoverSource.GetPixels32());
-		mapCover.Apply();
 
 		position = new Rect (0, 0, width, height);
-		visionPixels = (mapCover.height + mapCover.width) / 200 * visionPercentage;
+		visionPixels = (map.height + map.width) / 200 * visionPercentage;
+		refWidth = map.width / (refObjBounds.max.x - refObjBounds.min.x);
+		refHeight = map.height / (refObjBounds.max.z - refObjBounds.min.z);
 
-		refWidth = renderer.bounds.max.x - renderer.bounds.min.x;
-		refHeight = renderer.bounds.max.z - renderer.bounds.min.z;
+		makeCircle ();
 	}
 
 	void OnGUI() {
 		GUI.DrawTexture (position, map);
-		GUI.DrawTexture (position, mapCover);
 	}
 
 	void Update () {
-		float px = GameObject.FindWithTag("Player").transform.position.x - renderer.bounds.min.x;
-		int x = Mathf.RoundToInt(px * mapCover.width / refWidth);
-		float pz = GameObject.FindWithTag("Player").transform.position.z - renderer.bounds.min.z;
-		int y = Mathf.RoundToInt(pz * mapCover.height / refHeight);
+		int x = Mathf.RoundToInt((PlayerPos.x - refObjBounds.min.x) * refWidth);
+		int y = Mathf.RoundToInt((PlayerPos.z - refObjBounds.min.z) * refHeight);
 
-		paintCircle (mapCover, x, y, visionPixels, Color.clear);
+		paintCircle (x, y);
 	}
 
-	void paintCircle(Texture2D tex, int cx, int cy, int r, Color col)
-	{
-		for(int x= cx - r; x < cx+r; x++)
-		for(int y= cy - r; y < cy+r; y++) {
-			
-			float dx = x-cx, dy=y-cy;
+	void makeCircle() {
+		int r = visionPixels/2;
+		for(int x = 0; x < visionPixels; x++)
+		for(int y = 0; y < visionPixels; y++) {
+			float dx = x-r, dy=y-r;
 			float dist = Mathf.Sqrt(dx*dx+dy*dy);
 			
-			if(dist<r)
-				tex.SetPixel(x, y, col);
+			alpha[y * x + x] = Mathf.Min(0, ((r - dist) / r * 255) );
 		}
+	}
 
-		tex.Apply ();
+	void paintCircle(int cx, int cy)
+	{
+
 	}
 }
