578ec0b7e41a5227f5b1e976f30ca07d2a6127f2
Kei Hitomi <khitomi337@gmail.com>
2014-11-11 04:30:22 -0500

    github being dumb
    asking me to add more things that I have already added

---
+++ Demo Assets/Assets/Scripts/Ghost/GhostB.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostB : GenericGhost {
+	public Transform throughWall;
+	public Transform endSpot;
+	public float walkTime;
+	public float smooth;
+	private float endWalk;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!triggered){
+			endWalk = walkTime + walkTime;
+		}
+		if(triggered){
+			transform.position = Vector3.Lerp(transform.position, throughWall.position, smooth * Time.deltaTime);
+			if(Time.time > endWalk){
+				transform.position = endSpot.position;
+			}
+		}
+	}
+}
