520a01fd00b948fbf8509329909bb7f064c9aab9
kxh337 <khitomi337@gmail.com>
2014-11-23 03:35:44 -0500

    added more prefabs and things to the scene
    more ghost functionality and sound objects.  added prefabs for the stuff
    i'm working on

---
+++ Demo Assets/Assets/Scripts/ForceTurn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceTurn : MonoBehaviour {
+	public RoomTrigger room;
+	public AudioClip audio;
+	public AudioSource audioSource;
+	public float turnDuration;
+	private float turnTime;
+	// Use this for initialization
+	void Start () {
+		audioSource.clip = audio;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time < turnTime)
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player" && !room.roomExited){
+			turnPlayer(other.gameObject);
+		}
+	}
+
+	void turnPlayer(GameObject player){
+		MouseLook mouse = player.GetComponent<MouseLook>();
+		CharacterMotor motor = player.GetComponent<CharacterMotor>();
+		mouse.enabled = false;
+		motor.enabled = false;
+		turnTime = Time.time + turnDuration;
+		player.
+	}
+}
---
+++ Demo Assets/Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomTrigger : MonoBehaviour {
+	public bool roomEntered;
+	public bool roomExited;
+	// Use this for initialization
+	void Start () {
+		roomEntered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			roomEntered = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "Player"){
+			roomExited = true;
+		}
+	}
+}
