9837691502ce184b91dc1e22d1735f88ae0c65a6
Matheus Portillo <mathportillo@gmail.com>
2014-11-24 15:10:45 -0500

    Fixed pause menu positioning

--- Demo Assets/Assets/Scripts/Pause.cs
+++ Demo Assets/Assets/Scripts/Pause.cs
@@ -3,11 +3,10 @@ using System.Collections;
 
 public class Pause : MonoBehaviour {
 
-
-	private int buttonWidth = Screen.width / 6;
-	private int buttonHeight = Screen.height / 11;
-	private int groupWidth = Screen.width / 6;
+	private float buttonSize = (float) Screen.width / 1500;
+	private int groupWidth = Screen.width / 5;
 	private int groupHeight = Screen.height / 2;
+	private int space;
 	private bool gamePaused = false; 
 	public Texture2D quitText;
 	public Texture2D loadText;
@@ -20,9 +19,23 @@ public class Pause : MonoBehaviour {
 	public Texture2D optionsGlow;
 	public Texture2D resumeGlow;
 	public Texture2D saveGlow;
+	Rect[] rects;
+
 	void Start() 
 	{
 		Screen.lockCursor = true;
+		space = groupHeight / 5;
+		rects = new Rect[5];
+		Debug.Log (buttonSize * loadText.height);
+		Debug.Log (loadText.height);
+		Debug.Log (buttonSize * quitText.height);
+		Debug.Log (quitText.height);
+		rects[0] = new Rect((groupWidth - loadText.width) / 2, 0 * space, buttonSize * loadText.width, buttonSize * loadText.height);
+		rects[1] = new Rect((groupWidth - resumeText.width) / 2, 1 * space, buttonSize * resumeText.width, buttonSize * resumeText.height);
+		rects[2] = new Rect((groupWidth - saveText.width) / 2, 2 * space, buttonSize * saveText.width, buttonSize * saveText.height);
+		rects[3] = new Rect((groupWidth - optionsText.width) / 2, 3 * space, buttonSize * optionsText.width, buttonSize * optionsText.height);
+		rects[4] = new Rect((groupWidth - quitText.width) / 2, 4 * space, buttonSize * quitText.width, buttonSize * optionsText.height);
+		
 	}
 
 	// Update is called once per frame
@@ -40,27 +53,24 @@ public class Pause : MonoBehaviour {
 		if(gamePaused) 
 		{
 			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), pauseScreen); 
-			GUI.BeginGroup(new Rect(((Screen.width / 2) - (groupWidth / 2)), ((Screen.height / 2) - (groupHeight / 2) + Screen.height / 9),
+			GUI.BeginGroup(new Rect((Screen.width - groupWidth) / 2, (Screen.height - groupHeight) / 2 + Screen.height / 9,
 			                        groupWidth, groupHeight));
 			Event ev = Event.current;
 
-			Rect loadRect = new Rect(0, 0, buttonWidth / 2, buttonHeight * 0.9f);
-
-			if(ev != null && loadRect.Contains(ev.mousePosition))
+			if(ev != null && rects[0].Contains(ev.mousePosition))
 			{
-				GUI.DrawTexture(loadRect, loadGlow);
+				GUI.DrawTexture(rects[0], loadGlow);
 				if(ev.isMouse && ev.type == EventType.mouseUp)
 				{
 					//Do Load stuff here
 				}
-
 			}
 			else 
-				GUI.DrawTexture(loadRect, loadText);
-			Rect resumeRect = new Rect(0, buttonHeight + Screen.height / 50, buttonWidth, buttonHeight);
-			if(ev != null && resumeRect.Contains(ev.mousePosition))
+				GUI.DrawTexture(rects[0], loadText);
+
+			if(ev != null && rects[1].Contains(ev.mousePosition))
 			{
-				GUI.DrawTexture(resumeRect, resumeGlow);
+				GUI.DrawTexture(rects[1], resumeGlow);
 				if(ev.isMouse && ev.type == EventType.mouseUp)
 				{
 					gamePaused = false;
@@ -68,43 +78,40 @@ public class Pause : MonoBehaviour {
 				}
 			}
 			else
-				GUI.DrawTexture(resumeRect, resumeText);
+				GUI.DrawTexture(rects[1], resumeText);
 
-			Rect saveRect = new Rect(0, 2 * (buttonHeight + Screen.height / 50), buttonWidth / 2, buttonHeight * 0.9f);
-			if(ev != null && saveRect.Contains(ev.mousePosition))
+			if(ev != null && rects[2].Contains(ev.mousePosition))
 			{
-				GUI.DrawTexture(saveRect, saveGlow);
+				GUI.DrawTexture(rects[2], saveGlow);
 				if(ev.isMouse && ev.type == EventType.mouseUp)
 				{
 					//Do save stuff here
 				}
 			}
 			else
-				GUI.DrawTexture(saveRect, saveText);
+				GUI.DrawTexture(rects[2], saveText);
 
-			Rect optionsRect = new Rect(0, 3 * (buttonHeight + Screen.height / 50), buttonWidth, buttonHeight);
-			if(ev != null && optionsRect.Contains(ev.mousePosition))
+			if(ev != null && rects[3].Contains(ev.mousePosition))
 			{
-				GUI.DrawTexture(optionsRect, optionsGlow);
+				GUI.DrawTexture(rects[3], optionsGlow);
 				if(ev.isMouse && ev.type == EventType.mouseUp)
 				{
 					//Do options stuff here
 				}
 			}
 			else
-				GUI.DrawTexture(optionsRect, optionsText);
+				GUI.DrawTexture(rects[3], optionsText);
 
-			Rect quitRect = new Rect(0, 4 * (buttonHeight + Screen.height / 50), buttonWidth / 2, buttonHeight * 0.9f);
-			if(ev != null && quitRect.Contains(ev.mousePosition))
+			if(ev != null && rects[4].Contains(ev.mousePosition))
 			{
-				GUI.DrawTexture(quitRect, quitGlow);
+				GUI.DrawTexture(rects[4], quitGlow);
 				if(ev.isMouse && ev.type == EventType.mouseUp)
 				{
 					//Do save stuff here
 				}
 			}
 			else
-				GUI.DrawTexture(quitRect, quitText);
+				GUI.DrawTexture(rects[4], quitText);
 			GUI.EndGroup();
 		}
 	}
