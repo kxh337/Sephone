d168b134acd2040d0de09f1543913bcb753240af
Kei Hitomi <khitomi337@gmail.com>
2014-11-25 01:59:55 -0500

    fixed the red dead script

---
+++ Demo Assets/Assets/Scripts/CheckPointManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointManager : MonoBehaviour {
+	CheckPoint[] points;
+	private CheckPoint currentCheckPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void updateCheckPoint(){
+		for(CheckPoint point in points){
+
+		}
+	}
+}
