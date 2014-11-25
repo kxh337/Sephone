ebab6fb1370ab3f57c4e8db81a07ab3654f7ccac
Kei Hitomi <khitomi337@gmail.com>
2014-11-25 02:42:56 -0500

    added more checkpoint behavior
    it updates the check point (not very optimal) and then loads the player
    back if game over is true

--- Demo Assets/Assets/Scripts/CheckPointManager.cs
+++ Demo Assets/Assets/Scripts/CheckPointManager.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class CheckPointManager : MonoBehaviour {
+	public CheckPoint[] points;
+	public RedDead death;
+	private CheckPoint currentCheckPoint;
+	public GameObject player;
 	// Use this for initialization
 	void Start () {
 	
@@ -9,10 +13,17 @@ public class CheckPointManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		updateCheckPoint();
+		if(death.gameOver){
+			player.transform = currentCheckPoint;
+		}
 	}
 
 	void updateCheckPoint(){
-
+		foreach(CheckPoint point in points){
+			if(point.activated){
+				currentCheckPoint = point;
+			}
+		}
 	}
 }
