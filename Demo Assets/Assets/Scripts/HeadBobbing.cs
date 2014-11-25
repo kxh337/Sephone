163228a941fa796979d67d4fb00a7867d68ad112
Matheus Portillo <mathportillo@gmail.com>
2014-11-25 01:34:38 -0500

    Fixed bobbing

--- Demo Assets/Assets/Scripts/HeadBobbing.cs
+++ Demo Assets/Assets/Scripts/HeadBobbing.cs
@@ -7,41 +7,46 @@ public class HeadBobbing : MonoBehaviour {
 	public float bobbingAmount = 0.09F; 
 	//public float midpoint = 2.0F;
 	private float midpoint;
+	CharacterController controller;
 
 	void Start() {
 		midpoint = transform.localPosition.y + bobbingAmount;
-
+		controller = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float waveslice = 0.0F; 
-		float horizontal = Input.GetAxis("Horizontal"); 
-		float vertical = Input.GetAxis("Vertical"); 
-		if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0) { 
-			timer = 0.0F;
-		} 
-		else { 
-			waveslice = Mathf.Sin(timer); 
-			timer = timer + bobbingSpeed; 
-			if (timer > Mathf.PI * 2) { 
-				timer = timer - (Mathf.PI * 2); 
+		if(GameObject.Find("Totem").GetComponent<GamePause>().canPaused == 0) {
+
+			float waveslice = 0.0F; 
+			Vector3 horizontalVelocity = new Vector3(controller.velocity.x, 0, controller.velocity.z);
+			float horizontal = horizontalVelocity.magnitude; 
+			float vertical = controller.velocity.y;
+
+			if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0) { 
+				timer = 0.0F;
+			} else { 
+				waveslice = Mathf.Sin(timer); 
+				timer = timer + bobbingSpeed * controller.velocity.magnitude / 7; 
+				if (timer > Mathf.PI * 2) { 
+					timer = timer - (Mathf.PI * 2); 
+				} 
+			}
+			if (waveslice != 0) { 
+				float translateChange = waveslice * bobbingAmount; 
+				float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical); 
+				totalAxes = Mathf.Clamp (totalAxes, 0.0F, 1.0F); 
+				translateChange = totalAxes * translateChange;
+				transform.localPosition = new Vector3(translateChange,
+			                                        midpoint - Mathf.Abs(translateChange),
+				                                    0);
+			} else {
+				transform.localPosition = new Vector3(0,
+				                                      midpoint,
+				                                      0);
 			} 
-		} 
-		if (waveslice != 0) { 
-			float translateChange = waveslice * bobbingAmount; 
-			float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical); 
-			totalAxes = Mathf.Clamp (totalAxes, 0.0F, 1.0F); 
-			translateChange = totalAxes * translateChange;
-			transform.localPosition = new Vector3(translateChange,
-		                                        midpoint - Mathf.Abs(translateChange),
-			                                    0);
-		} 
-		else {
-			transform.localPosition = new Vector3(0,
-			                                      midpoint,
-			                                      0);
-		} 
+
+		}
 	
 	}
 }
