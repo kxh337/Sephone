7a43ca59682b963db5f26b8e6d9182ffa4238088
Jiawei He <ehiewaij.jh@gmail.com>
2014-11-24 22:29:17 -0500

    Added yoyo trigger

---
+++ Demo Assets/Assets/Scripts/YoYoTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YoYoTrigger : MonoBehaviour {
+	public AudioSource source;
+	public AudioClip sound;
+	private bool played;
+	private bool containsYoYo;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player" && !played){
+			audio.Play();
+			//containsYoYo = GameObject.FindGameObjectWithTag("Inventory").SendMessage("InventoryContain", 6);
+			played = true;
+		}
+	}
+}
