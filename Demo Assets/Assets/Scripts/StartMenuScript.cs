0d84bce3a23d06d1c3837371f089c890f297875a
Jiawei He <ehiewaij.jh@gmail.com>
2014-11-25 02:00:17 -0500

    Added start menu stuff

--- Demo Assets/Assets/Scripts/StartMenuScript.cs
+++ Demo Assets/Assets/Scripts/StartMenuScript.cs
@@ -13,6 +13,7 @@ public class StartMenuScript : MonoBehaviour {
 	public Texture2D background;
 	Rect creditsRect;
 	Rect helpRect;
+	Rect newgameRect;
 	// Use this for initialization
 	void Start () {
 
@@ -20,12 +21,21 @@ public class StartMenuScript : MonoBehaviour {
 	void OnGUI()  
 	{
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
-		//GUI.DrawTexture(new Rect(0, Screen.height * 0.8f, background.width, background.height), background);
+		Event e = Event.current;
+		if(newgameRect.Contains(e.mousePosition))
+		{
+			GUI.DrawTexture(newgameRect, newgame);
+			if(e.isMouse && e.type == EventType.mouseUp)
+				Application.LoadLevel(1);
+		}
+		else
+			GUI.DrawTexture(newgameRect, newgameRed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Rect creditsRect = new Rect(Screen.width - credits.width, Screen.height * 0.8f, credits.width, credits.height);
 		Rect helpRect = new Rect(Screen.width - help.width, Screen.height * 0.8f + creditsRect.height, help.width, help.height);
+		newgameRect = new Rect(0, Screen.height * 0.8f - newgame.height, newgame.width, newgame.height);
 	}
 }
