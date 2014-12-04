c3f6e6632da8f5b135c41675b3813bb4fdd7c075
Matheus Portillo <mathportillo@gmail.com>
2014-12-04 09:11:37 -0500

    fixed map error; adding natural movement script

---
+++ Demo Assets/Assets/Scripts/NaturalMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NaturalMovement : MonoBehaviour {
+	public GameObject reference;
+	public GameObject player;
+	public float verticalPercentage = 30;
+	public float horizontalPercentage = 30;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//		float newx = reference.transform.rotation.x * verticalPercentage / 100;
+//		float newz = player.rigidbody.angularVelocity.y * horizontalPercentage / 100;
+////		Debug.Log(newx);
+//		Debug.Log(reference.transform.rotation.x + " " + newx);
+//		transform.localRotation = new Quaternion (	newx,
+//		                                		    transform.rotation.y,
+//				                                    newz,
+//				                                    transform.rotation.w);
+//		                                    
+//	}
+}
