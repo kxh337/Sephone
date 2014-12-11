c8f0b22f06962c40108b1effdd70da9fb089879e
Kei Hitomi <khitomi337@gmail.com>
2014-12-11 16:58:37 -0500

    fixed locket placement
    locket is placement is fixed and item glow prefab and a candle light
    prefab is added

---
+++ Demo Assets/Assets/ItemGlow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemGlow : MonoBehaviour {
+	public Light glow;
+	public float glowSpeed;
+	public float glowMin;
+	public float glowMax;
+	private float glowVal;
+	private int glowDirection;
+	// Use this for initialization
+	void Start () {
+		glowDirection = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		glowVal = glow.range;
+		if(glowVal > glowMax){
+			glowDirection = -1;
+		}
+		else if(glowVal < glowMin){
+			glowDirection = 1;
+		}
+		glow.range =  glow.range + glowSpeed * glowDirection * Time.deltaTime;
+	}
+}
